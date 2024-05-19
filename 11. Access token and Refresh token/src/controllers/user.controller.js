e6a74145beb41d3ce68718612416a6844ef9ef70
charu <charupsharma5@gmail.com>
2024-05-19 11:34:30 +0530

    refreshAccessToken endpoint route made in user.routes.js

--- 11. Access token and Refresh token/src/controllers/user.controller.js
+++ 11. Access token and Refresh token/src/controllers/user.controller.js
@@ -263,7 +263,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 })
 
 
-export { registerUser, loginUser, logoutUser }
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
 
 
 
--- 11. Access token and Refresh token/src/routes/user.routes.js
+++ 11. Access token and Refresh token/src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -19,9 +20,10 @@ router.route("/register").post(
     ]),//middleware ends here
     registerUser)
 
-
 router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 export default router
