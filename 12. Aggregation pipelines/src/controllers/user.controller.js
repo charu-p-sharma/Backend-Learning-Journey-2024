2cb0a0403b9a0790faa7bbe2c101fb180aac4045
charu <charupsharma5@gmail.com>
2024-05-22 11:22:08 +0530

    Validation added in generateAccessAndRefreshTokens method in controller

--- 12. Aggregation pipelines/src/controllers/user.controller.js
+++ 12. Aggregation pipelines/src/controllers/user.controller.js
@@ -11,6 +11,9 @@ import mongoose from "mongoose";
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
         const user = await User.findById(userId);
+        if (!user) {
+            throw new ApiError(404, "User not found !")
+        }
         const accessToken = user.generateAccessToken()
         const refreshToken = user.generateRefreshToken()
 
